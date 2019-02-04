a45c38476643fd54bbdf46d10e8d250d601b9bd3
Sofia2703 <flr_3214@hotmail.co.th>
2019-02-04 15:29:26 +0700

    เพิ่ม + ทดสอบ route กดแล้วมา แต่มาซ้อนกันเหมือนแค่โชว panel

---
+++ src/app/test-material/test-material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TestMaterialRoutingModule } from './test-material-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TestMaterialRoutingModule
+  ]
+})
+export class TestMaterialModule { }
